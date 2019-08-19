de474094e90b728c5aaeb76a3b5121fde5d43a61
Rovian Vieceli <rovianvieceli@gmail.com>
2019-08-19 14:04:21 -0300

    Uso de classes. Exemplos de Herança e classe estática

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-alert('Inicio');
+// Classe
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);        
+        console.log(this.data);
+    }
+}
+
+// Classe: Herança
+class TodoList extends List {
+    constructor() {
+        // Chamada do construtor do pai
+        super()
+
+        this.usuario = "Mark";
+    }
+
+    mostraUsuaio() {
+        console.log(this.usuario);
+    }
+}
+
+// Classe: Estática    
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+// Chamada e utilização das Classe 
+const MinhaLista = new TodoList();
+MinhaLista.mostraUsuaio();
+document.getElementById('novoTodo').onclick = function() {
+    MinhaLista.add('New Todo');
+}
+
+// Estática não precisa do New
+console.log(Matematica.soma(1, 3));
+
---
+++ bundle.js
@@ -0,0 +1,96 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+// Classe
+var List =
+/*#__PURE__*/
+function () {
+  function List() {
+    _classCallCheck(this, List);
+
+    this.data = [];
+  }
+
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
+  }]);
+
+  return List;
+}(); // Classe: Herança
+
+
+var TodoList =
+/*#__PURE__*/
+function (_List) {
+  _inherits(TodoList, _List);
+
+  function TodoList() {
+    var _this;
+
+    _classCallCheck(this, TodoList);
+
+    // Chamada do construtor do pai
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this));
+    _this.usuario = "Mark";
+    return _this;
+  }
+
+  _createClass(TodoList, [{
+    key: "mostraUsuaio",
+    value: function mostraUsuaio() {
+      console.log(this.usuario);
+    }
+  }]);
+
+  return TodoList;
+}(List); // Classe: Estática    
+
+
+var Matematica =
+/*#__PURE__*/
+function () {
+  function Matematica() {
+    _classCallCheck(this, Matematica);
+  }
+
+  _createClass(Matematica, null, [{
+    key: "soma",
+    value: function soma(a, b) {
+      return a + b;
+    }
+  }]);
+
+  return Matematica;
+}(); // Chamada e utilização das Classe 
+
+
+var MinhaLista = new TodoList();
+MinhaLista.mostraUsuaio();
+
+document.getElementById('novoTodo').onclick = function () {
+  MinhaLista.add('New Todo');
+}; // Estática não precisa do New
+
+
+console.log(Matematica.soma(1, 3));
