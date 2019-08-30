c64e24de1d536efe440754291c8821fab206cf2e
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-08-30 13:12:15 -0300

    Async/Await

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-alert('Run in Dev Mode');
+const MyPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { 
+        resolve('200 OK'); 
+        // reject('401 Unauthorized');
+    }, 2000);
+});
+
+/**
+ * Default sem Es6
+ */
+MyPromise()
+   .then(response => { console.log(response); })
+   .catch(error => { console.log(error); });   
