1fd381af3390d0ed23fd752a46fd524f1eaea2e8
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-09-02 12:47:12 -0300

    Merge axios

--- src/app.js
+++ src/app.js
@@ -1,13 +1,1 @@
-import axios from 'axios';
-
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);    
-            console.log(response);
-        } catch (error) {
-            console.log('Falha ao buscar as informações');
-        }       
-    }
-}
-// Api.getUserInfo('informe o username');
+alert('Start Dev');
