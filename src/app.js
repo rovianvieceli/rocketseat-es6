6e1b82d0b83de5f1703d7038157c23d96e589a7c
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-09-02 13:27:44 -0300

    template

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-alert('Start Dev');
+class App {
+    constructor() {
+        this.repositories = [];
+        this.form = document.getElementById('repo-form');
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.form.onsubmit = event => this.addRepository();
+    }
+
+    addRepository(event){        
+        event.preventDefault();
+    }
+}
