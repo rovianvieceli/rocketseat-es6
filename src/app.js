6fa4b17c30fecd0c3c2878247a159d99bcc8da00
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-09-03 14:15:01 -0300

    adicionando repositorios

--- src/app.js
+++ src/app.js
@@ -6,10 +6,21 @@ class App {
     }
 
     registerHandlers() {
-        this.form.onsubmit = event => this.addRepository();
+        this.form.onsubmit = event => this.addRepository(event);
     }
 
     addRepository(event){        
         event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire suas idéias do papel e de vida à sua StartUP',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http//github.com/rocketseat/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
     }
 }
+
+new App();
